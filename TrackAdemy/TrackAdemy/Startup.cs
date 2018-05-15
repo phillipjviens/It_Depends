7683a0abf2e543293878d86d02fa1ee01425c7f3
Brian <b.doersch5173@edmail.edcc.edu>
2018-05-14 23:22:29 -0700

    Adding project solution

---
+++ TrackAdemy/TrackAdemy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrackAdemy.Startup))]
+namespace TrackAdemy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
