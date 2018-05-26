cc4ed5f0718fcfae53aa76373422e0b729d517e9
Brian <doerschb@seattleu.edu>
2018-05-26 03:36:25 -0700

    stubbing out possible redirect action for later

--- TrackAdemy/TrackAdemy/Controllers/HomeController.cs
+++ TrackAdemy/TrackAdemy/Controllers/HomeController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrackAdemy.Backend;
+using TrackAdemy.Models;
 
 namespace TrackAdemy.Controllers
 {
+   
     public class HomeController : Controller
     {
+        // A ViewModel used for the Avatar that contains the AvatarList
+        private UserViewModel userViewModel = new UserViewModel();
+
+        // The Backend Data source
+        private UserBackend userBackend = UserBackend.Instance;
+
+        //GET: User
         public ActionResult Index()
         {
-            return View();
+            userViewModel.UserList = userBackend.Index();            
+            return View(userViewModel);
         }
 
         public ActionResult About()
@@ -29,8 +40,12 @@ namespace TrackAdemy.Controllers
         public ActionResult Help()
         {
             ViewBag.Message = "Your Help page.";
-
+            
             return View();
         }
+        //public ActionResult FormHandler(string username, int level)
+        //{
+        //    return RedirectToAction("Admin");
+        //}
     }
 }
