2623c2012c8658c074ffd28f88ac6a7c75c594e7
Brian <doerschb@seattleu.edu>
2018-05-31 13:32:20 -0700

    non student data source

--- TrackAdemy/TrackAdemy/Backend/NonStudentDataSourceMock.cs
+++ TrackAdemy/TrackAdemy/Backend/NonStudentDataSourceMock.cs
@@ -44,6 +44,86 @@ namespace TrackAdemy.Backend
         /// </summary>
         private List<AdminKioskModel> nonStudentList = new List<AdminKioskModel>();
 
+        /// <summary>
+        /// Makes a non Student
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns>non student Passed In</returns>
+        public AdminKioskModel Create(AdminKioskModel data)
+        {
+            nonStudentList.Add(data);
+            return data;
+        }
+
+
+        /// <summary>
+        /// Return the data for the id passed in
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Null or valid data</returns>
+        public AdminKioskModel Read(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return null;
+            }
+
+            var myReturn = nonStudentList.Find(n => n.Id == id);
+            return myReturn;
+        }
+
+        /// <summary>
+        /// Update all attributes to be what is passed in
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns>Null or updated data</returns>
+        public AdminKioskModel Update(AdminKioskModel data)
+        {
+            if (data == null)
+            {
+                return null;
+            }
+            var myReturn = nonStudentList.Find(n => n.Id == data.Id);
+
+            myReturn.Update(data);
+
+            return myReturn;
+        }
+
+        /// <summary>
+        /// Remove the Data item if it is in the list
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>True for success, else false</returns>
+        public bool Delete(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return false;
+            }
+
+            var myData = nonStudentList.Find(n => n.Id == id);
+            var myReturn = nonStudentList.Remove(myData);
+            return myReturn;
+        }
+
+        /// <summary>
+        /// Return the full dataset
+        /// </summary>
+        /// <returns>List of nonstudentusers</returns>
+        public List<AdminKioskModel> Index()
+        {
+            return nonStudentList;
+        }
+
+        /// <summary>
+        /// Reset the Data, and reload it
+        /// </summary>
+        public void Reset()
+        {
+            nonStudentList.Clear();
+            Initialize();
+        }
 
         /// <summary>
         /// Create Placeholder Initial Data
@@ -53,4 +133,5 @@ namespace TrackAdemy.Backend
             Create(new AdminKioskModel("stevesteve", "stevesteve", 2));
             Create(new AdminKioskModel("kioskkiosk", "kioskkiosk", 3));
         }
+    }
 }
