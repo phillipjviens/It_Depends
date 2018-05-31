3c945eb1cbcfed9403b1da2f3c2ecaa4948c32b1
Jake <laderah@seattleu.edu>
2018-05-31 12:35:53 -0700

    Created list of Students in StudentDataSourceMock.cs

--- TrackAdemy/TrackAdemy/Backend/StudentDataSourceMock .cs
+++ TrackAdemy/TrackAdemy/Backend/StudentDataSourceMock .cs
@@ -40,6 +40,12 @@ namespace TrackAdemy.Backend
         }
 
         /// <summary>
+        /// The User List
+        /// </summary>
+        private List<StudentModel> studentList = new List<StudentModel>();
+
+
+        /// <summary>
         /// Create Placeholder Initial Data
         /// </summary>
         public void Initialize()
