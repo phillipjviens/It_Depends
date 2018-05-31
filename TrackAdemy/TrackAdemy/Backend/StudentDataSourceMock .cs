3a83def0caff4efe892c64773de17947c3032b12
Jake <laderah@seattleu.edu>
2018-05-31 11:45:35 -0700

    Added the Singleton to StudentDataSourceMock.cs

--- TrackAdemy/TrackAdemy/Backend/StudentDataSourceMock .cs
+++ TrackAdemy/TrackAdemy/Backend/StudentDataSourceMock .cs
@@ -11,5 +11,32 @@ namespace TrackAdemy.Backend
     /// </summary>
     public class StudentDataSourceMock : IUserInterface
     {
+        /// <summary>
+        /// Make into a Singleton
+        /// </summary>
+        private static volatile StudentDataSourceMock instance;
+        private static object syncRoot = new Object();
+
+        private StudentDataSourceMock() { }
+
+        public static StudentDataSourceMock Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new StudentDataSourceMock();
+                            instance.Initialize();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
     }
 }
