eedbbaded12afee3b8a259d187da378b35ec4e12
Jake <laderah@seattleu.edu>
2018-05-31 11:38:30 -0700

    Added StudentDataMock.cs as the mock data for Students

---
+++ TrackAdemy/TrackAdemy/Backend/StudentDataSourceMock .cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using TrackAdemy.Models;
+namespace TrackAdemy.Backend
+{
+    public class StudentDataSourceMock : IUserInterface
+    {
+    }
+}
