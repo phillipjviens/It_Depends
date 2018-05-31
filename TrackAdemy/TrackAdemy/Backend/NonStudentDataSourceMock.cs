7654752d712d37ead15b3a101bd05f3a5787046d
Jake <laderah@seattleu.edu>
2018-05-31 11:42:25 -0700

    Added NonStudentDataSourceMock.cs.

---
+++ TrackAdemy/TrackAdemy/Backend/NonStudentDataSourceMock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using TrackAdemy.Models;
+namespace TrackAdemy.Backend
+{
+    /// <summary>
+    /// Backend Mock DataSource for NonStudent users, to manage them
+    /// </summary>
+    public class NonStudentDataSourceMock : IUserInterface
+    {
+    }
+}
