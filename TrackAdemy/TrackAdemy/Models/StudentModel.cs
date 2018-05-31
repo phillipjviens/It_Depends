a2b82b07253539958e992ec08f6efce2e0567c11
Jake <laderah@seattleu.edu>
2018-05-31 10:35:59 -0700

    Added ID attribute to StudentModel.cs

--- TrackAdemy/TrackAdemy/Models/StudentModel.cs
+++ TrackAdemy/TrackAdemy/Models/StudentModel.cs
@@ -4,9 +4,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TrackAdemy.Models
 {
+    /// <summary>
+    /// Students for the system
+    /// </summary>
     public class StudentModel
     {
-       
+        /// <summary>
+        /// Student GUID
+        /// </summary>
+        [Display(Name = "Id", Description = "Student Id")]
+        [Required(ErrorMessage = "Id is required")]
+        public string Id { get; set; }
+
+
     }
 }
 
