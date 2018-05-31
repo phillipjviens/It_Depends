b88633b4b3883d090e145fc4c78abf73a69d7106
Jake <laderah@seattleu.edu>
2018-05-31 11:43:37 -0700

    Added comment for StudentDataSourceMock class

--- TrackAdemy/TrackAdemy/Backend/StudentDataSourceMock .cs
+++ TrackAdemy/TrackAdemy/Backend/StudentDataSourceMock .cs
@@ -6,6 +6,9 @@ using System.Web;
 using TrackAdemy.Models;
 namespace TrackAdemy.Backend
 {
+    /// <summary>
+    /// Backend Mock DataSource for Student Users, to manage them
+    /// </summary>
     public class StudentDataSourceMock : IUserInterface
     {
     }
