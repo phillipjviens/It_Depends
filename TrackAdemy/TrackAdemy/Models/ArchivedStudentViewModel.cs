4198fe745ff751e3bb842fddfc767174e85fe999
Jake <laderah@seattleu.edu>
2018-06-05 12:06:29 -0700

    Added ArchivedStudentViewModel to models

---
+++ TrackAdemy/TrackAdemy/Models/ArchivedStudentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrackAdemy.Models
+{
+    /// <summary>
+    /// View Model for the Student Index
+    /// </summary>
+    public class StudentViewModel
+    {
+        /// <summary>
+        /// The student List to return to the View
+        /// </summary>
+        public List<StudentModel> StudentList = new List<StudentModel>();
+
+   
+    }
+}
